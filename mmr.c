28a0cd28ca9243ddd6b939b4780b4df45f7eb1a0
Richard Jennings <ricyje@protonmail.com>
2019-09-06 12:32:27 -0700

    add decay function, header, and fixed build.

--- mmr.c
+++ mmr.c
@@ -1,8 +1,13 @@
 #include "mmr.h"
 #include "cdf.h" //table that saves a lot of time
 #include <float.h>
-#define DISTVARNO 15 //this should be odd ideally
-#define DIST_ITER for(int x = 0; x!= DISVARNO; x++)
+#define DIST_ITER for(int x = 0; x!= DISTVARNO; x++)
+void decay(double * dist, double H)
+{
+	//this increases the effect of subsequent updates
+	DIST_ITER
+		dist[x] = (dist[x] + H)/(H*DISTVARNO)+1;
+}
 double cdf_i(double x, double theta, double mu)
 {
 	int location = (((x-mu)/theta)*500)+5000;
@@ -12,13 +17,22 @@ double cdf_i(double x, double theta, double mu)
 		return DBL_MIN;
 	return cdf_const[location];
 }
+double wAverage(double * dist)
+{
+	//gets raw mmr from a player or team curve
+	double res = 0;
+	DIST_ITER
+		res += x*dist[x];
+	return res;
+}
+
 void iniUniform(double * p)
 {
 	//needed for a new player
 	DIST_ITER
 		p[x] = 1.0/DISTVARNO;
 }
-void wPartyDist(double ** p_in , p_count, double * p_out)
+void wPartyDist(double ** p_in , int p_count, double * p_out)
 {
 	//init to 1
 	DIST_ITER
@@ -47,7 +61,6 @@ void updateMMR(double * dist,int outcome, double delta)
 		double p = cdf_i(x-mmr_effective, 1.0, 0.0);
 		dist[x]*= outcome?1.0-p:p; 
 		cumsum += dist[x];
-
 	};
 	//normalize
 	DIST_ITER
@@ -61,7 +74,6 @@ int processMatch(double ** p1, double** p2, int p_count)
 	wPartyDist(p1,p_count,p1_dist);
 	wPartyDist(p2,p_count,p2_dist);
 	double delta = wAverage(p1_dist) - wAverage(p2_dist);
-
 	//update each player distribution based on the effective mmr (player mmr + delta
 	for (int player = 0;player != p_count; player++)
 	{
@@ -71,18 +83,6 @@ int processMatch(double ** p1, double** p2, int p_count)
 		updateMMR(p2[player],0,-delta);
 	};
 	return 0;
-
 }
 
-double wAverage(double * dist)
-{
-	//gets raw mmr from a player or team curve
-	double res = 0;
-	DIST_ITER
-	{
-		res += x*dist[x];
-
-	};
-	return res;
-}
 
---
+++ mmr.h
@@ -0,0 +1,6 @@
+#define DISTVARNO 15 //this should be odd ideally
+
+void decay(double * dist, double H);
+double wAverage(double * dist);
+void iniUniform(double * p);
+int processMatch(double ** p1, double** p2, int p_count);
